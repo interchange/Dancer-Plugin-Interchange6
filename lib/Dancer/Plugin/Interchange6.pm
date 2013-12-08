04c950baeb8a6678f30fcea8105f3d5ba4897fa3
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-12-08 12:30:04 +0100

    Bump up version number.

--- lib/Dancer/Plugin/Interchange6.pm
+++ lib/Dancer/Plugin/Interchange6.pm
@@ -17,11 +17,11 @@ Dancer::Plugin::Interchange6 - Interchange6 Shop Plugin for Dancer
 
 =head1 VERSION
 
-Version 0.002
+Version 0.003
 
 =cut
 
-our $VERSION = '0.002';
+our $VERSION = '0.003';
 
 =head1 ROUTES
 
