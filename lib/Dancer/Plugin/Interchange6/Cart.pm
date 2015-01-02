c9c55d07009aa798e6c2cfb45ca0f3d40777bfd2
Stefan Hornburg (Racke) <racke@linuxia.de>
2015-01-02 09:56:17 +0100

    Update copyright years.

--- lib/Dancer/Plugin/Interchange6/Cart.pm
+++ lib/Dancer/Plugin/Interchange6/Cart.pm
@@ -670,7 +670,7 @@ Receives: $cart, $old_name, $new_name
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2011-2014 Stefan Hornburg (Racke) <racke@linuxia.de>.
+Copyright 2011-2015 Stefan Hornburg (Racke) <racke@linuxia.de>.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
