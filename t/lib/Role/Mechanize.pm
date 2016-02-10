8b49459a98f5ac2254ddae98b39834a6d1a93bc1
Peter Mottram <peter@sysnix.com>
2016-02-10 12:37:08 +0100

    use lazy build for Mech test mech attribute and add clearer

--- t/lib/Role/Mechanize.pm
+++ t/lib/Role/Mechanize.pm
@@ -2,25 +2,25 @@ package Role::Mechanize;
 
 use Moo::Role;
 
-use Dancer qw/:syntax/;
+use Dancer ();
 use Test::WWW::Mechanize::PSGI;
 use Types::Standard qw/InstanceOf/;
 
-use namespace::clean;
-
 has mech => (
-    is  => 'lazy',
-    isa => InstanceOf ['Test::WWW::Mechanize'],
-    default => sub {
-        Test::WWW::Mechanize::PSGI->new(
-            app => sub {
-                my $env = shift;
-                load_app 'TestApp',;
-                my $request = Dancer::Request->new( env => $env );
-                Dancer->dance( $request );
-            }
-        );
-    },
+    is      => 'lazy',
+    isa     => InstanceOf ['Test::WWW::Mechanize'],
+    clearer => 1,
 );
+
+sub _build_mech {
+    Test::WWW::Mechanize::PSGI->new(
+        app => sub {
+            my $env = shift;
+            Dancer::load_app 'TestApp',;
+            my $request = Dancer::Request->new( env => $env );
+            Dancer->dance($request);
+        }
+    );
+}
 
 1;
